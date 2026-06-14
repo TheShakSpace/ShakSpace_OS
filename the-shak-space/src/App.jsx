193e9e1b12052a177e9c368c08f7250da6d8af48
Shakshi <shakshikumari2541@gmail.com>
2026-06-15 00:02:42 +0530

    day 1 done

--- the-shak-space/src/App.jsx
+++ the-shak-space/src/App.jsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from "react";
+import SplashScreen from "./components/splash/SplashScreen";
+import AppLayout from "./components/layout/AppLayout";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 4000); // 4 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center">
-      <h1 className="text-5xl font-bold">
-        Hello Shak Space
-      </h1>
-    </div>
-  )
+    <>
+      {loading ? <SplashScreen /> : <AppLayout />}
+    </>
+  );
 }
 
-export default App
+export default App;
